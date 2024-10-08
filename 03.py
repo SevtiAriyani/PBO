29095d864815ed887976b3d0273e639f47f0aacd
SevtiAriyani <sevtiariyani2424@gmail.com>
2024-10-08 20:25:22 +0700

    Update 03.py

--- 03.py
+++ 03.py
@@ -11,9 +11,9 @@ class Mahasiswa:
         print(f"{self.nama} sedang belajar.")
 
 # Implementasi penggunaan kelas Mahasiswa
-mahasiswa1 = Mahasiswa("Sevti Ariyani", "202301110007", "Informatika")
-mahasiswa2 = Mahasiswa("Ariyani Sevti", "202301110000", "Sistem Informasi")
+mahasiswa1 = Mahasiswa("Sevti", "202301110007", "Informatika")
+mahasiswa2 = Mahasiswa("Ariyani", "202301110007", "Sistem Informasi")
 
 # Menggunakan metode objek
-mahasiswa1.belajar()  # Output: Budi sedang belajar.
-mahasiswa2.belajar()  # Output: Ani sedang belajar.
+mahasiswa1.belajar()  # Output: Sevti sedang belajar.
+mahasiswa2.belajar()  # Output: Ariyani sedang belajar.
